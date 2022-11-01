4e34b9ee0e00b4ec9e7bbdc2e7308f3d1c8aced5
user <raphels123@gmail.com>
2022-10-31 22:06:54 -0300

    other commit

--- orientacaoObjetoAvancado/modelos.py
+++ orientacaoObjetoAvancado/modelos.py
@@ -1,3 +1,4 @@
+import random
 class Programa():
     def __init__(self,nome,ano):
         self._nome = nome.title()
@@ -18,32 +19,60 @@ class Programa():
         self._nome = novo_nome.title() 
 
 
-
-
-
 class Filme(Programa):
     def __init__(self,nome,ano,duracao):
-        self._nome = nome.title()
-        self.ano = ano
+        super().__init__(nome,ano)
         self.duracao = duracao
-        self._likes = 0
-        
+    
+    
+    def __str__(self):
+        duracaoRound = round(self.duracao/60)
+        return f'{self.nome} - {self.ano} -  {duracaoRound} horas - {self.likes} Likes'  
         
 class Serie(Programa): 
     def __init__(self,nome,ano,temporadas):
-        self._nome = nome.title()
-        self.ano = ano
+        super().__init__(nome,ano)
         self.temporadas = temporadas
-        self._likes = 0
+    
+    def __str__(self):    
+        return f'{self.nome} - {self.ano} -  {self.temporadas} temporadas - {self.likes} Likes'
+    
+class Playlist:
+    def __init__(self,nome,programas):
+        self.nome = nome
+        self.programas = programas
+    
+    
+    def tamanho(self):
+        return len(self.programas)
+
+                       
     
            
             
         
 vingadores = Filme('vingadores', 2018 , 160)
 barbaros = Serie('barbaros', 2022, 5)
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('demolidor',2018, 2)
+
+
+demolidor.dar_like()
 vingadores.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
 vingadores.dar_like()
 vingadores.dar_like()
 barbaros.dar_like()
-print(f'nome: {barbaros.nome} Ano: {barbaros.ano} temporadas: {barbaros.temporadas} likes: {barbaros.likes}')
-print(f'nome: {vingadores.nome} Ano: {vingadores.ano} duracao: {vingadores.duracao} likes: {vingadores.likes}')
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+
+filmes_e_series = [vingadores,barbaros,demolidor,tmep]
+
+plys_fim_de_semana = Playlist('fim de semana' , filmes_e_series)
+
+for i in plys_fim_de_semana.programas:
+    print(i)
